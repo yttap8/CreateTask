2799498ac1a0c0b66a924fd4f0648c1529b8c074
yttap8 <moc.liamg.patrick@gmail.com>
2022-03-21 16:05:38 -0400

    messy stuff

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var board = document.getElementById('board')
+
 var vx = 1, vy = 1;
 // (4, 4)
 var snake = [35];
@@ -6,8 +8,12 @@ for (var i = 0; i<63; i++){
   snake.push()
 }
 
+
 function main() {
 
 }
 
 setInterval(main, 1000/20)
+
+
+function randomFood()
