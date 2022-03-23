429ed3f852eb51c0e626f0a44b70d450b51d5e8f
yttap8 <moc.liamg.patrick@gmail.com>
2022-03-23 01:27:53 -0400

    small

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 var board = document.getElementById('board')
-
 var vx = 1, vy = 1;
 // (4, 4)
 var snake = [35];
 
 for (var i = 0; i<63; i++){
-  snake.push()
+  snake.push()//not sure if this is the right way to approach...
 }
 
 
@@ -16,4 +15,6 @@ function main() {
 setInterval(main, 1000/20)
 
 
-function randomFood()
+function randomFood(){
+
+}
