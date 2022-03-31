bd8ba7cfbf9050fc296b2b91beb1fb59880b8d45
yttap8 <moc.liamg.patrick@gmail.com>
2022-03-30 21:55:43 -0400

    tictactoe

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-var board = document.getElementById('board')
+var board = [
+  ['','','']
+  ['','','']
+  ['','','']
+];
+
+var move = ['X','O'];
+
 var vx = 1, vy = 1;
 // (4, 4)
 var snake = [35];
@@ -9,12 +16,15 @@ for (var i = 0; i<63; i++){
 
 
 function main() {
+  if (event.key == ArrowLeft){
 
+  }
+  if (event.key == ArrowUp){
+  }
+  if (event.key == ArrowRight){
+  }
+  if (event.key == ArrowDown){
+  }
 }
 
 setInterval(main, 1000/20)
-
-
-function randomFood(){
-
-}
