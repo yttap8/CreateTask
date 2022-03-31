4b7888ce5558c2ff0e67fcc25673e2527f76d0d8
yttap8 <moc.liamg.patrick@gmail.com>
2022-03-30 23:02:25 -0400

    grid

--- main.js
+++ main.js
@@ -6,25 +6,20 @@ var board = [
 
 var move = ['X','O'];
 
-var vx = 1, vy = 1;
-// (4, 4)
-var snake = [35];
+function check() {
+  for (var r = 0; r<3; r++)
+    for (var c = 0; c<3;c++)
+      if (board[r][c]) = move){
 
-for (var i = 0; i<63; i++){
-  snake.push()//not sure if this is the right way to approach...
+      }
 }
 
 
-function main() {
-  if (event.key == ArrowLeft){
 
-  }
-  if (event.key == ArrowUp){
-  }
-  if (event.key == ArrowRight){
-  }
-  if (event.key == ArrowDown){
-  }
+
+
+if (move == 'X'){
+  move == 'O';
+}else{
+  move = 'X'
 }
-
-setInterval(main, 1000/20)
